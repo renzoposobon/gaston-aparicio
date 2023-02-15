447e42958c250c9323d416ac672dd205d5524807
Renzo Posobon <renzoposobon95@gmail.com>
2023-02-15 10:20:41 -0300

    wave index

--- src/componentes/Inicio.jsx
+++ src/componentes/Inicio.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import wave from "../recursos/wave.svg"
 
 const Inicio = () => {
 
@@ -8,10 +9,14 @@ const Inicio = () => {
         <img src={require("../recursos/foto-sin-fondo-con-alta-calidad-recortada.png")} alt="foto-gaston" className='imagenInicio' />
         {/* <p className='slogan'>#ElijoCreer ⭐⭐⭐</p> */}
         {/* <img src={require("../recursos/DSC00434.png")} alt="foto-gaston" className='imagenInicio' /> */}
+        {/* <img src={wave} alt="" className='wave' /> */}
         <article className='cuerpoTextoInicio'>
-          <p className='tituloUno'>¡Hola!</p>
-          <p className="tituloInicio">Soy Gastón Aparicio</p>
-          <p className='tituloAbajo'>Quiero darte la bienvenida a mi sitio web <i className='icono-peronista'>✌</i></p>
+          <img src={wave} alt="" className='wave' />
+          <div>
+            <p className='tituloUno'>¡Hola!</p>
+            <p className="tituloInicio">Soy Gastón Aparicio</p>
+            <p className='tituloAbajo'>Quiero darte la bienvenida a mi sitio web <i className='icono-peronista'>✌</i></p>
+          </div>
         </article>
       </main>
     </div>
